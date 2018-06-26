066dd30a3c3ab6f0ef2ea736503cc327055cdb11
YanaLitvinchik <litvinchikyana@ukr.net>
2018-06-26 04:32:43 +0300

    Done

--- CurrencyCalculator/MainWindow.xaml.cs
+++ CurrencyCalculator/MainWindow.xaml.cs
@@ -25,9 +25,61 @@ namespace CurrencyCalculator
             InitializeComponent();
         }
 
+        private void Convert()
+        {
+            if (grn.Text.LastOrDefault() != ',' && euro.Text.LastOrDefault() != ',' && dollar.Text.LastOrDefault() != ',' && rub.Text.LastOrDefault() != ',')
+            {
+                if (grn.IsFocused && grn.Text != String.Empty)
+                {
+                    euro.Text = $"{Math.Round(30.52 * Double.Parse(grn.Text), 2)}";
+                    dollar.Text = $"{Math.Round(26.32 * Double.Parse(grn.Text), 2)}";
+                    rub.Text = $"{Math.Round(0.42 * Double.Parse(grn.Text), 2)}";
+                }
+                else if (euro.IsFocused && euro.Text != String.Empty)
+                {
+                    grn.Text = $"{Math.Round(30.52 * Double.Parse(euro.Text), 2)}";
+                    dollar.Text = $"{Math.Round(1.16 * Double.Parse(euro.Text), 2)}";
+                    rub.Text = $"{Math.Round(72.69 * Double.Parse(euro.Text), 2)}";
+                }
+                else if (dollar.IsFocused && dollar.Text != String.Empty)
+                {
+                    grn.Text = $"{Math.Round(26.32 * Double.Parse(dollar.Text), 2)}";
+                    euro.Text = $"{Math.Round(0.86 * Double.Parse(dollar.Text), 2)}";
+                    rub.Text = $"{Math.Round(62.69 * Double.Parse(dollar.Text), 2)}";
+                }
+                else if (rub.IsFocused && rub.Text != String.Empty)
+                {
+                    grn.Text = $"{Math.Round(0.42 * Double.Parse(rub.Text), 2)}";
+                    euro.Text = $"{Math.Round(0.01 * Double.Parse(rub.Text), 2)}";
+                    dollar.Text = $"{Math.Round(0.02 * Double.Parse(rub.Text), 2)}";
+                }
+                else
+                    grn.Text = euro.Text = dollar.Text = rub.Text = "";
+            }
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            Convert();
+        }
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
            
+        }
+
+        private void grn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void rub_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void euro_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
